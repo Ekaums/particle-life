f4f4c5077aed4b9471e60637b0c75ff11f4400c6
Ekaums <ekaum.singh@gmail.com>
2024-11-27 00:22:38 -0500

    scuffed pause/play

--- src/driver.cpp
+++ src/driver.cpp
@@ -14,34 +14,43 @@ int main(){
     init(window, render);
 
     bool quit{false};
-    SDL_Event e;
+    bool playing{true};
+    SDL_Event event;
 
     std::vector<Particle> particles;
     Timer timer;
 
-    for(int i = 0; i < 100; i++){
+    for(int i = 0; i < 1000; i++){
       particles.push_back(Particle(SCREEN_W/2, SCREEN_H/2));
     }
 
     while(!quit){
       
-        while(SDL_PollEvent(&e) != 0){
+        while(SDL_PollEvent(&event) != 0){
             // User request quit
-            if(e.type == SDL_QUIT)
-            quit = true;
+            if(event.type == SDL_QUIT)
+              quit = true;
+
+            else if(event.type == SDL_KEYDOWN){
+              if(event.key.keysym.sym == SDLK_SPACE){
+                playing = !playing;
+                std::cout << "pause/play\n";
+              }
+            }
         }
 
         /* Game loop */
+        if(playing){
+          // Clean screen
+          SDL_SetRenderDrawColor(render, 0, 0, 0, 255); // Black
+          SDL_RenderClear(render);
 
-        // Clean screen
-        SDL_SetRenderDrawColor(render, 0, 0, 0, 255); // Black
-        SDL_RenderClear(render);
-
-        for(Particle& p : particles){
-          p.Move();
-          p.Draw(render, 255, 0, 0);
+          for(Particle& p : particles){
+            p.Move();
+            p.Draw(render, 255, 0, 0);
+          }
+          SDL_RenderPresent(render);
         }
-        SDL_RenderPresent(render);
     }
 
     SDL_DestroyRenderer(render);
